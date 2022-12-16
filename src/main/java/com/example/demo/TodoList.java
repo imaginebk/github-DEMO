0d025f9cd68976a8a4b139a87dab13c8447d9f6a
imaginebk <kimhisung246@gmail.com>
2022-12-16 19:25:47 +0900

    살려주시라요

---
+++ src/main/java/com/example/demo/TodoList.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class TodoList {
+
+    
+    public String helloWorld(){
+        System.out.println("to-do Application" );
+        
+        return null;
+    }
+
+}
